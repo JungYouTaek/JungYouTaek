07a47192a8e4e469beacb9db03a670de7e50d60e
JungYouTaek <jyt9319@gmail.com>
2017-05-15 22:11:54 +0900

    edit navbar / sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,45 @@
 <template>
-  <v-app>
+  <v-app with top-fixed-tollbar left-fixed-sidebar bottom-footer>
     <Navbar/>
-    <v-content>
+    <main>
+      <Sidebar/>
+      <v-content fluid>
       <nuxt/>
-      <Foot/>
-    </v-content>
+      </v-content>
+    </main>
+    <Foot/>
+
   </v-app>
 </template>
 
 <script>
 import Navbar from '~components/Nav/Navbar.vue'
+import Sidebar from '~components/Nav/Sidebar.vue'
 import Foot from '~components/Foot.vue'
 
 export default {
   components: {
     Navbar,
+    Sidebar,
     Foot
   }
 }
 </script>
 <style>
-
+html, body {
+  height: 100%;
+}
+.content {
+  height: 100%;
+}
+#app {
+  height: 100%;
+}
+.with {
+  min-height: 100vh;
+  overflow-x: hidden;
+}
+.with.top-fixed-toolbar {
+  padding-top: 64px;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,37 @@
 <template lang="html">
-
+  <v-carousel icon="remove">
+    <v-carousel-item v-for="(item,i) in items" v-bind:src="item.src" :key="i" />
+  </v-carousel>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      items: [
+        { src: 'about.svg' },
+        { src: 'check.svg' }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="css">
+  .carousel {
+    width: 100%;
+    height: 600px;
+  }
+  @media (max-width: 400px) {
+    .carousel {
+      width: 100%;
+      height: 600px;
+    }
+  }
+  @media (max-width: 320px) {
+    .carousel {
+      width: 100%;
+      height: 460px;
+    }
+  }
 </style>
