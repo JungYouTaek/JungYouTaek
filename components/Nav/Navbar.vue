54441c6fde81074646b7aa961ffd8cdd201521a5
JungYouTaek <jyt9319@gmail.com>
2017-05-17 06:39:22 +0900

    edit menu buttom

--- components/Nav/Navbar.vue
+++ components/Nav/Navbar.vue
@@ -6,13 +6,32 @@
       <v-spacer></v-spacer>
       <v-toolbar-item href="https://github.com/JungYouTaek/nuxt-practice" ripple><img src="/github.svg" alt="github-logo"></v-toolbar-item>
       <v-toolbar-item href="/contact" ripple><img src="/speech-bubble.svg" alt="contact us"></v-toolbar-item>
-      <v-toolbar-item ripple>Link</v-toolbar-item>
+      <v-menu bottom right>
+        <v-btn icon="icon" slot="activator" dark>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-item v-for="item in items" :key="item">
+            <v-list-tile>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-toolbar>
   </nav>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      items: [
+        { title: '회원가입' },
+        { title: '개인정보' }
+      ]
+    }
+  }
 }
 </script>
 
@@ -30,5 +49,11 @@ a:not(:hover) {
 }
 li {
   list-style: none;
+}
+.list__tile__title {
+  color: #8bc34a;
+}
+.list__item a:hover {
+  color: green;
 }
 </style>
